bc1b2b172b663456a156869ecddb384527f4b229
fresk <fresk93@gmail.com>
2020-03-15 12:48:36 +0300

    Add filter

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,16 @@
   <div id="app">
     <h1>Todo application</h1>
     <AddTodo @add-todo="addTodo" />
+    <select v-model="filter">
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+      <option value="not-completed">Not completed</option>
+    </select>
     <hr>
-    <TodoList v-bind:todos="todos" @remove-todo="removeTodo" />
+    <TodoList
+      v-bind:todos="filteredTodos"
+      @remove-todo="removeTodo"
+    />
   </div>
 </template>
 
@@ -19,7 +27,21 @@ export default {
   },
   data: () => {
     return {
-      todos: []
+      todos: [],
+      filter: 'all'
+    }
+  },
+  computed: {
+    filteredTodos() {
+      if (this.filter === 'completed') {
+        return this.todos.filter(t => t.completed);
+      }
+
+      if (this.filter === 'not-completed') {
+        return this.todos.filter(t => !t.completed);
+      }
+
+      return this.todos;
     }
   },
   mounted() {
@@ -29,7 +51,7 @@ export default {
   },
   methods: {
     removeTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      this.todos = this.todos.filter(t => t.id !== id);
     },
     addTodo(todo) {
       this.todos.push(todo);

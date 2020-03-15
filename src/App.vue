4ba9e1c489cbd30e4fce04079aa3fd458da6126e
fresk <fresk93@gmail.com>
2020-03-15 13:09:13 +0300

    Add routing

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,10 @@
 <template>
   <div id="app">
     <h1>Todo application</h1>
-    <AddTodo @add-todo="addTodo" />
-    <select v-model="filter">
-      <option value="all">All</option>
-      <option value="completed">Completed</option>
-      <option value="not-completed">Not completed</option>
-    </select>
     <hr>
-    <Loader v-if="loading" />
-    <TodoList
-      v-else-if="filteredTodos.length"
-      v-bind:todos="filteredTodos"
-      @remove-todo="removeTodo"
-    />
-    <p v-else>No todos!</p>
+    <router-view />
   </div>
 </template>
-
-<script>
-import TodoList from './components/TodoList';
-import AddTodo from './components/AddTodo';
-import Loader from './components/Loader';
-
-export default {
-  name: 'App',
-  components: {
-    TodoList,
-    AddTodo,
-    Loader
-  },
-  data: () => {
-    return {
-      todos: [],
-      loading: true,
-      filter: 'all'
-    }
-  },
-  computed: {
-    filteredTodos() {
-      if (this.filter === 'completed') {
-        return this.todos.filter(t => t.completed);
-      }
-
-      if (this.filter === 'not-completed') {
-        return this.todos.filter(t => !t.completed);
-      }
-
-      return this.todos;
-    }
-  },
-  mounted() {
-    fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
-      .then(response => response.json())
-      .then(json => {
-        this.todos = json;
-        this.loading = false;
-      })
-  },
-  methods: {
-    removeTodo(id) {
-      this.todos = this.todos.filter(t => t.id !== id);
-    },
-    addTodo(todo) {
-      this.todos.push(todo);
-    }
-  }
-}
-</script>
 
 <style>
 #app {
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur corporis cumque deleniti eligendi error et inventore iusto minus possimus quam quas, qui quibusdam quis repellendus sequi suscipit ut! Fugit, molestias.</p>
+        <router-link to="/todos">todos</router-link>
+    </div>
+</template>
